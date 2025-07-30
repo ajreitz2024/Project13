f3c4a5474ab1dd9493fb4a7ed686e3468fced2da
ajreitz2024 <ajreitz2024@gmail.com>
2025-07-30 08:12:49 -0400

    Wrote function "character" to test if the start character is a letter and if the target character is a letter. Function throws exceptions if not.

--- Project13/Exceptions.cpp
+++ Project13/Exceptions.cpp
@@ -7,6 +7,20 @@
 using namespace std;
 
 char character(char start, int offset) {
+	if (!isalpha(start)) {
+		throw "invalidCharacterException";
+	}
+	int target = start;
+	target = target + offset;
+	if (target >= 65 && target <= 90) {
+		char targetChar = static_cast<char>(target);
+	}
+	else if (target >= 97 && target <= 122) {
+		char targetChar = static_cast<char>(target);
+	}
+	else {
+		throw "invalidRangeException";
+	}
 }
 
 int main() {
