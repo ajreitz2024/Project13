e2073ca1c0305cf3e7dde89be1ff4ec82f755b06
ajreitz2024 <ajreitz2024@gmail.com>
2025-07-30 09:06:39 -0400

    Added comments to code

--- Project13/Exceptions.cpp
+++ Project13/Exceptions.cpp
@@ -6,37 +6,37 @@
 
 using namespace std;
 
-char character(char start, int offset) {
-	if (!isalpha(start)) {
-		throw "invalidCharacterException";
+char character(char start, int offset) { // character function
+	if (!isalpha(start)) { // if not a character:
+		throw "invalidCharacterException"; // throw an exception
 	}
-	int target = start;
-	target = target + offset;
-	if (target >= 65 && target <= 90) {
-		char targetChar = static_cast<char>(target);
-		if (islower(start) && isupper(targetChar)) {
-			throw "invalidRangeException";
+	int target = start; // make the start value an ASCII value
+	target = target + offset; // add the offset value
+	if (target >= 65 && target <= 90) { // if the target ASCII value is between A and Z (in ASCII):
+		char targetChar = static_cast<char>(target); // turn target to a character
+		if (islower(start) && isupper(targetChar)) { // if lower to upper conversion:
+			throw "invalidRangeException"; // throw exception
+		}
+		else { 
+			return targetChar; // return target character
+		}
+	}
+	else if (target >= 97 && target <= 122) { //if the target ASCII value is between a and z (in ASCII):
+		char targetChar = static_cast<char>(target); // turn target to character
+		if (isupper(start) && islower(targetChar)) { // if upper to lower conversion:
+			throw "invalidRangeException"; // throw exception
 		}
 		else {
-			return targetChar;
-		}
-	}
-	else if (target >= 97 && target <= 122) {
-		char targetChar = static_cast<char>(target);
-		if (isupper(start) && islower(targetChar)) {
-			throw "invalidRangeException";
-		}
-		else {
-			return targetChar;
+			return targetChar; // return target character
 		}
 	}
 	else {
-		throw "invalidRangeException";
+		throw "invalidRangeException"; // throw exception if not in those ASCII values
 	}
 }
 
 int main() {
-	try {
+	try { 
 		cout << character('a', 1) << endl;
 	}
 	catch (...) {
